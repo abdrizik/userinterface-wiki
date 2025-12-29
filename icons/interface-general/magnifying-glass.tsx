e7bc2cfe3bb9623fcb22be59d94291304052c265
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2025-12-29 21:17:31 +0000

    style(icon): fix SVG attribute casing for consistency

--- icons/interface-general/magnifying-glass.tsx
+++ icons/interface-general/magnifying-glass.tsx
@@ -17,14 +17,14 @@ export const MagnifyingGlassIcon = ({
     <path
       d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18Z"
       stroke={color}
-      stroke-width="2"
-      stroke-linecap="round"
+      strokeWidth="2"
+      strokeLinecap="round"
     />
     <path
       d="M20 20L16.05 16.05"
       stroke={color}
-      stroke-width="2"
-      stroke-linecap="round"
+      strokeWidth="2"
+      strokeLinecap="round"
     />
   </svg>
 );
